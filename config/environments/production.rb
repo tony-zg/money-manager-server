2dacda154d574c87c642122f729b7f48bcbcc51b
Tony Guan <tonyg06@msn.com>
2020-02-28 09:18:59 +1100

    updating gem file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  gem 'rails_12factor', group: :production
 end
